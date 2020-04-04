0927e347e291beabc8bf11add6b9ca30604baa24
unknown <jake_stetter@hotmail.com>
2020-04-04 13:19:41 -0700

    update

--- flask/flask/templates/logic.js
+++ flask/flask/templates/logic.js
@@ -1,7 +1,7 @@
 //Create map object
 var myMap = L.map("map", {
-    center: [37.7749, -122.4194],
-    zoom: 13
+    center: [39.8097343, -98.5556199],
+    zoom: 3
   });
 
 // Create map background
@@ -12,3 +12,12 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: API_KEY
 }).addTo(myMap);
 
+
+var cases_path = '../../../data/time_series_covid19_confirmed_US.geojson'
+var deaths_path = '../../../data/time_series_covid19_deaths_US.geojson'
+
+cases = d3.json(cases_path)
+deaths = d3.json(deaths_path)
+
+console.log(cases)
+console.log(deaths)
